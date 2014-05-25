7b9a00ffd96f60a397afd3c5bdad1d8789a77fc3
Ethan Kang <ethan.yskang@gmail.com>
2014-05-25 09:58:31 -0400

    Testing

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ## initialize ma to NULL
+        ma <- NULL
+        
+        ## set function takes in a matrix, and set x equal to it
+        ## also set ma to NULL
+        set <- function(y) {
+                x <<- y
+                ma <<- NULL
+        }
+        
+        ## get function just returns x that was assigned in set(y)
+        get <- function() x
+        
+        ##setcache function sets ma equal to cache
+        setcache <- function(cache) ma <<- cache
+        
+        ##getcache function just returns ma
+        getcache <- function() ma
+        list(set = set, get = get,
+             setcache = setcache,
+             getcache = getcache)
 }
 
 
@@ -12,4 +32,21 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        ##getcache of matrix x. if it is not NULL
+        ##then use the cached inverted matrix
+        ma <- x$getcache()
+        if(!is.null(ma)) {
+                message("getting cached data")
+                return(ma)
+        }
+        
+        ##if it is not NULL, get() to get the matrix that was 
+        ##input in the makeCacheMatrix
+        data <- x$get()
+        
+        ##assign ma to inverted matrix, set it to cache
+        ma <- solve(data, ...)
+        x$setcache(ma)
+        ma
 }
